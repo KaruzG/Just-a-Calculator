4e47638527614ecaa193937ad3af326f9896821c
KaruzG <josemiguelcpcs@gmail.com>
2022-07-28 16:57:49 +0200

    aa

---
+++ dia1.js
@@ -0,0 +1,6 @@
+console.log("dia1.js loaded!")
+function suma() {
+    let num1 = parseInt(document.getElementById("numero1").value, 10);
+    let num2 = parseInt(document.getElementById("numero2").value, 10);
+    document.getElementById("resultado").innerHTML = num1 + num2;
+}

3915fc5f98b5be5826d312d049edfd8f6cca6692
KaruzG <josemiguelcpcs@gmail.com>
2022-08-01 04:27:47 +0200

    Calculator layout, now needs functions

--- dia1.js
+++ dia1.js
@@ -4,3 +4,9 @@ function suma() {
     let num2 = parseInt(document.getElementById("numero2").value, 10);
     document.getElementById("resultado").innerHTML = num1 + num2;
 }
+
+function resta() {
+    let num1 = parseInt(document.getElementById("numero1").value, 10);
+    let num2 = parseInt(document.getElementById("numero2").value, 10);
+    document.getElementById("resultado").innerHTML = num1 - num2;
+}

95197f82a52519164111c331e08f671c4a185480
KaruzG <josemiguelcpcs@gmail.com>
2022-08-03 02:31:58 +0200

    Added numbers functions

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-console.log("dia1.js loaded!")
-function suma() {
-    let num1 = parseInt(document.getElementById("numero1").value, 10);
-    let num2 = parseInt(document.getElementById("numero2").value, 10);
-    document.getElementById("resultado").innerHTML = num1 + num2;
-}
+//--------------------------------------------------------------|
+//----------             KaruzG on GitHub             ----------|
+//--------------------------------------------------------------|
 
-function resta() {
-    let num1 = parseInt(document.getElementById("numero1").value, 10);
-    let num2 = parseInt(document.getElementById("numero2").value, 10);
-    document.getElementById("resultado").innerHTML = num1 - num2;
-}
+console.log("main.js loaded!");
+
+var num1 = "";
+var num2 = "";
+var secondNumberTrigger = false; // When a operator is clicked, this will set True, to num2 hold the second number.
+
+function addChar(num){  // Func when a number is clicked
+    num1 = num1 + num.toString();
+    console.log(num);
+    console.log(num1);
+    document.getElementById("resultInput").value = num1;
+};
+
+function remove(){  // Resets all
+    num1 = "";
+    num2 = "";
+    secondNumberTrigger = false;
+    document.getElementById("resultInput").value = num1
+};
